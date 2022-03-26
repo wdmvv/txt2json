df53834c6bb7508e5f1310fe1de477a87383ec9b
~wdmvv <78714307+wdmvv@users.noreply.github.com>
2022-03-26 02:33:01 +0000

    Update deff.go

--- rest/deff.go
+++ rest/deff.go
@@ -3,9 +3,22 @@ package rest
 import "fmt"
 
 func DefPath() (string, string) {
-	var name string
+	
+	var (
+		name string
+		named string
+	)
+
 	fmt.Print("File name from this directory: ")
 	fmt.Scanln(&name)
-	namj := name[:len(name)-3] + "json"
-	return name, namj
+	
+	for i := 0; i < len(name); i++{
+		if string(name[i]) == "."{
+			break
+		} else {
+			named += string(name[i])
+		}
+	}
+
+	return name, named + ".json"
 }
